e9bea9794724b866639bef2ac2d6c48133b39cb6
Nikita Andrushko <alemkhf12@gmail.com>
2023-10-18 22:59:11 +0300

    [RarefiedMatrix] Start

---
+++ DS-Lab/RarefiedMatrix/RarefiedMatrix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class RarefiedMatrix
+{
+    private List<RarefiedMatrix> _upNodesList;
+    private List<RarefiedMatrix> _leftNodesList;
+
+    public int Rows => _leftNodesList.Count;
+    public int Columns => _upNodesList.Count;
+    
+    public RarefiedMatrix(int rows, int columns)
+    {
+        _leftNodesList = new List<RarefiedMatrix>(rows);
+        _upNodesList = new List<RarefiedMatrix>(columns);
+    }
+}
---
+++ DS-Lab/RarefiedMatrix/RarefiedMatrixNode.cs
@@ -0,0 +1,7 @@
+﻿public class RarefiedMatrixNode
+{
+    public int Value { get; set; }
+    
+    public RarefiedMatrixNode? RightNode { get; set; }
+    public RarefiedMatrixNode? DownNode { get; set; }
+}
