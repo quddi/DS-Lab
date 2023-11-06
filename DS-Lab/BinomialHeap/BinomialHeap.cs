14b29f21284670d5ea331f233e5746c9734ec23f
Nikita Andrushko <alemkhf12@gmail.com>
2023-11-06 23:00:57 +0200

    [BinomialHeap] Start

---
+++ DS-Lab/BinomialHeap/BinomialHeap.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+public class BinomialHeap
+{
+    private BinomialHeapNode? _firstTreeRoot;
+    
+    public override string ToString()
+    {
+        if (_firstTreeRoot == null)
+            return "Empty binomial heap."; 
+        
+        var stringBuilder = new StringBuilder();
+
+        while (_firstTreeRoot != null)
+        {
+            
+        }
+        
+        return stringBuilder.ToString();
+    }
+}
---
+++ DS-Lab/BinomialHeap/BinomialHeapNode.cs
@@ -0,0 +1,14 @@
+﻿public class BinomialHeapNode
+{
+    public int Key { get; set; }
+    
+    public int Value { get; set; }
+    
+    public int Degree { get; set; }
+
+    public BinomialHeapNode? Parent { get; set; }
+    
+    public BinomialHeapNode? Child { get; set; }
+    
+    public BinomialHeapNode? Sibling { get; set; }
+}
